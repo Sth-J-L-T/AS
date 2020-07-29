d4348fcf62d692542cd200f2bd0608da5e497551
huzhuorong <3467283952@qq.com>
2020-07-29 14:04:54 +0800

    第二次在AS上传git,这里是分支hot_fix

--- AS/app/src/main/java/com/example/as_git/MainActivity.java
+++ AS/app/src/main/java/com/example/as_git/MainActivity.java
@@ -11,5 +11,6 @@ protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String a = "AS第一次上传";
+        String b = "在分支hot_fix上修改";
     }
 }
