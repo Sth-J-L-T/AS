aa757b151c340b2312da9c1878db4d5ee2e69257
huzhuorong <3467283952@qq.com>
2020-07-29 13:04:30 +0800

    第一次在AS上传git

---
+++ AS/app/src/main/java/com/example/as_git/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.as_git;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String a = "AS第一次上传";
+    }
+}
